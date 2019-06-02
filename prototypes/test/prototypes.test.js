f10995729e41e3a9c7488b135c4930ae07d52d24
EduRomoGO <eduromogo@outlook.es>
2019-06-02 14:54:26 +0200

    feat: add more examples to prototype

--- prototypes/test/prototypes.test.js
+++ prototypes/test/prototypes.test.js
@@ -28,7 +28,50 @@ describe('prototypes', () => {
             const mestalla = new Stadium('mestalla');
             
             expect(mestalla.createMatch('celta')).to.equal('Valencia against celta');
-        });    
+        });
+
+        it('prototype is shared between objects created by same constructor function', () => {
+            //Create an empty constructor function
+            function Person(){
+            }
+
+            //Add property name, age to the prototype property of the Person constructor function
+            Person.prototype.name = "Edu" ;
+            Person.prototype.age = 26;
+
+            //Create objects using the Person constructor function
+            var person1= new Person();
+            var person2 = new Person();
+
+            expect(person1.prototype).to.equal(person2.prototype);
+        });
+        
+        it('any modification to prototype props affect every object having this prototype', () => {
+            //Create an empty constructor function
+            function Person(){
+            }
+            //Add property name, age to the prototype property of the Person constructor function
+            Person.prototype.name = "Edu" ;
+            Person.prototype.age = 26;
+            Person.prototype.friends = ['Juanmi'] //Arrays are of reference type in JavaScript
+
+            //Create objects using the Person constructor function
+            var person1 = new Person();
+            var person2 = new Person();
+
+            // Add a new element to the friends array. Here you are not modifying the reference
+            person1.friends.push("Alejandro");
+
+            expect(person1.friends).to.equal(person2.friends);
+
+            // Here the reference is modified, so friends prototype property is not affected. Instead a property friends is created inside person1
+            person1.friends = [...person1.friends, 'Edu'];
+
+            expect(person1.hasOwnProperty('friends')).to.be.true;
+            expect(person1.friends).to.deep.equal(['Juanmi', 'Alejandro', 'Edu']);
+            expect(person2.friends).to.deep.equal(['Juanmi', 'Alejandro']);
+        });
+        
     });
 
 });
