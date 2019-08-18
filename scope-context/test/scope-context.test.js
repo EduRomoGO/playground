bbf5950105d91ceb8514f29d55aeb7d0d42576d4
EduRomoGO <eduromogo@outlook.es>
2019-08-18 12:34:24 +0200

    test

--- scope-context/test/scope-context.test.js
+++ scope-context/test/scope-context.test.js
@@ -3,22 +3,33 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('scope-context', () => {
+    const painter = {
+        owner: 'Jose',
+        paint: function paintHouse() {
+            function paintRoof(msg) {
+                return `${msg} and im painting the roof of ${this.owner}'s house`;
+            }
+
+            return paintRoof(`im painting ${this.owner} house`);
+        }
+    }
 
     it('loses the scope of `this` when it is used inside of a function that is contained inside of another function', () => {
-        const painter = {
-            owner: 'Jose',
-            paint: function paintHouse() {
-                function paintRoof(msg) {
-                    return `${msg} and im painting the roof of ${this.owner}'s house`;
-                }
-
-                return paintRoof(`im painting ${this.owner} house`);
-            }
-        }
-
         expect(painter.paint()).to.equal(`im painting Jose house and im painting the roof of undefined's house`);
     });
 
-    // it('we can use bind to `Function.prototype.bind` to bind `this` to paintRoof')
+    it('we can use bind to `Function.prototype.bind` to bind `this` to paintRoof', () => {
+        painter.paint = function paintHouse() {
+            function paintRoof(msg) {
+                return `${msg} and im painting the roof of ${this.owner}'s house`;
+            }
 
+            // Create a new function with `this` bound to it
+            const boundPaintRoof = paintRoof.bind(this);
+
+            return boundPaintRoof(`im painting ${this.owner} house`);
+        };
+
+        expect(painter.paint()).to.equal(`im painting Jose house and im painting the roof of Jose's house`);
+    });
 });
