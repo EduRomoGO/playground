4a87120520d532a72a3bdcb44091680e1de4d378
EduRomoGO <eduromogo@outlook.es>
2019-10-27 16:09:07 +0100

    chore: add

---
+++ async/src/async.js
@@ -0,0 +1,10 @@
+const myData = { post: 100};
+
+const fetchDataFromApi = () => Promise.resolve(myData);
+// const fetchDataFromApi = () => {
+//     fetch(url)
+//         .then()
+//         .catch();
+// }
+
+
