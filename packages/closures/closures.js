2ce2f73f013fd816583a571443bfa4a77e4b589b
EduRomoGO <eduromogo@outlook.es>
2019-11-01 13:45:08 +0100

    feat: extract and add more info

--- packages/closures/closures.js
+++ packages/closures/closures.js
@@ -1,12 +1,4 @@
-// Todo list example
-const todoAppFactory = () => {
-    const todos = [];
-
-    const add = todo => todos.push(todo);
-    const counter = () => todos.length;
-
-    return { add, counter };
-};
+const todoAppFactory = require('./todo.js');
 
 const myTodoApp = todoAppFactory();
 
