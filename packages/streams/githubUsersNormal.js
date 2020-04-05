b5735b016252e46379aea1c3dc69097154239260
EduRomoGO <eduromogo@outlook.es>
2020-04-05 17:04:52 +0100

    feat: get github users; normal

--- packages/streams/githubUsersNormal.js
+++ packages/streams/githubUsersNormal.js
@@ -1,7 +1,47 @@
 import axios from './node_modules/axios';
 
 export const loadUsers = async () => {
-    const users = await axios.get('https://api.github.com/users');
+    const { data: users } = await axios.get('https://api.github.com/users');
 
-    console.log(users.data.length);
+    const getRandomUser = users => {
+        return users[Math.floor(Math.random() * users.length)];
+    }
+
+    const user1 = getRandomUser(users)
+    const user2 = getRandomUser(users)
+
+    const fillData = (user, number) => {
+        const userNode = document.querySelector(`.user${number}`);
+
+        userNode.querySelector('img').src = user ? user.avatar_url : ''
+        userNode.querySelector('.name').textContent = user ? user.login : ''
+    }
+    
+    document.querySelector('button').addEventListener('click', () => {
+        fillData(null, 1)
+        fillData(null, 2)    
+
+        setTimeout(() => {
+            const user1 = getRandomUser(users)
+            const user2 = getRandomUser(users)
+        
+            fillData(user1, 1)
+            fillData(user2, 2)    
+        }, 300);
+    });
+
+    [...document.querySelectorAll('span')].map(n => n.addEventListener('click', (event) => {
+        fillData(null, event.target.dataset.id)
+        setTimeout(() => {
+            const user = getRandomUser(users)
+    
+            fillData(user, event.target.dataset.id)
+        }, 300);
+    }))
+
+    fillData(user1, 1)
+    fillData(user2, 2)
+
+    console.log(user1)
+    console.log(user2)
 }
