a01f76d3d3f79c9e5b6a1c0977f57464df687ea1
EduRomoGO <eduromogo@outlook.es>
2021-05-24 16:23:51 +0200

    docs

--- packages/cookies/index.js
+++ packages/cookies/index.js
@@ -5,8 +5,8 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   res.setHeader("Set-Cookie", [
-    "type=ninja; Max-Age=20",
-    "language=javascript",
+    "type=ninja; Max-Age=200; HttpOnly",
+    "language=javascript; Path=/other; HttpOnly", // esta cookie solo puede verse en las dev tools tras ir a / y luego a /other. En / se setea la cookie, y luego ya solo se envia al hacer peticion a /other
   ]);
   res.send("Hello World!");
 });
